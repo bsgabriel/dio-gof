aa8568db44dad9f4dc1b2a7126696fe99b28fc56
bsgabriel <gabrielbs22@icloud.com>
2025-05-09 02:48:13 -0300

    Ajuste de formatação

--- src/main/java/bsg/gof/service/cliente/ClienteService.java
+++ src/main/java/bsg/gof/service/cliente/ClienteService.java
@@ -2,9 +2,9 @@ package bsg.gof.service.cliente;
 
 import bsg.gof.dto.ClienteDto;
 import bsg.gof.dto.ClienteRequestDto;
-import bsg.gof.model.Cliente;
 
 public interface ClienteService {
+
     ClienteDto inserir(ClienteRequestDto cliente);
 
 }
--- src/main/java/bsg/gof/service/infocep/InfoCepService.java
+++ src/main/java/bsg/gof/service/infocep/InfoCepService.java
@@ -3,5 +3,7 @@ package bsg.gof.service.infocep;
 import bsg.gof.dto.CepDto;
 
 public interface InfoCepService {
+
     CepDto buscarDadosCep(Long cep);
+
 }
